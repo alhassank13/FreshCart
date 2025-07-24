704c1288339a0b83bd848176ca0df4a0b656c35b
alhassank13 <alhassankhaled24@gmail.com>
2025-07-24 15:04:34 +0300

    checkout address

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
           path: "/categories/:id",
           element: (
             <ProtetedRoutes>
-              <SpecificCategory/>
+              <SpecificCategory />
               <SubCategories />
             </ProtetedRoutes>
           ),
